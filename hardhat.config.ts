52d809b12de3dd1be19705ba6e91ac306a1751c3
bob <>
2023-04-06 16:02:16 +0800

    fix: add fantom testnet deploy config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,14 +2,19 @@ import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY
+const ANKR_API_KEY = process.env.ANKR_API_KEY
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 
-module.exports = {
+https: module.exports = {
   solidity: '0.8.18',
   networks: {
     goerli: {
       url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [PRIVATE_KEY],
+    },
+    fantom_testnet: {
+      url: `https://rpc.ankr.com/fantom_testnet/${ANKR_API_KEY}`,
       accounts: [PRIVATE_KEY],
     },
   },
