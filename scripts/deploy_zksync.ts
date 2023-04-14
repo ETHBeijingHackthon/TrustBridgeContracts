192120933bf3617740d30c593706b3adaf5642fb
bob <>
2023-04-14 17:27:07 +0800

    add zksync deploy script

---
+++ scripts/deploy_zksync.ts
@@ -0,0 +1,27 @@
+import { utils, Wallet } from 'zksync-web3'
+import * as ethers from 'ethers'
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { Deployer } from '@matterlabs/hardhat-zksync-deploy'
+
+// An example of a deploy script that will deploy and call a simple contract.
+export default async function (hre: HardhatRuntimeEnvironment) {
+  console.log(`Running deploy script for the Greeter contract`)
+
+  // Initialize the wallet.
+  const private_key = process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY : ''
+  const wallet = new Wallet(private_key)
+
+  // Create deployer object and load the artifact of the contract we want to deploy.
+  const deployer = new Deployer(hre, wallet)
+  const artifact = await deployer.loadArtifact('TrustBridge')
+
+  // Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
+  // `greeting` is an argument for contract constructor.
+  //   const greeting = 'Hi there!'
+  //   const greeterContract = await deployer.deploy(artifact, [greeting])
+  const greeterContract = await deployer.deploy(artifact)
+
+  // Show the contract info.
+  const contractAddress = greeterContract.address
+  console.log(`${artifact.contractName} was deployed to ${contractAddress}`)
+}
