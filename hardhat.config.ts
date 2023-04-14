b9bd593452db4f9778c75e904f46c77fdc535415
bob <>
2023-04-14 17:19:38 +0800

    add zksync network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,8 @@
 import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
+import '@matterlabs/hardhat-zksync-deploy'
+import '@matterlabs/hardhat-zksync-solc'
+import '@matterlabs/hardhat-zksync-verify'
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY
 const ANKR_API_KEY = process.env.ANKR_API_KEY
@@ -8,6 +11,12 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 https: module.exports = {
   solidity: '0.8.18',
+  zksolc: {
+    version: '1.3.5',
+    compilerSource: 'binary',
+    settings: {},
+  },
+  defaultNetwork: 'zkTestnet',
   networks: {
     goerli: {
       url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
@@ -17,6 +26,20 @@ https: module.exports = {
     fantom_testnet: {
       url: `https://rpc.ankr.com/fantom_testnet/${ANKR_API_KEY}`,
       accounts: [PRIVATE_KEY],
+    },
+    zkTestnet: {
+      url: 'https://testnet.era.zksync.dev', // URL of the zkSync network RPC
+      ethNetwork: 'goerli', // Can also be the RPC URL of the Ethereum network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+      zksync: true,
+      verifyURL:
+        'https://zksync2-testnet-explorer.zksync.dev/contract_verification',
+    },
+    zkMainnet: {
+      url: 'https://mainnet.era.zksync.io',
+      ethNetwork: 'https://eth.llamarpc.com',
+      zksync: true,
+      verifyURL:
+        'https://zksync2-mainnet-explorer.zksync.io/contract_verification',
     },
   },
   etherscan: {
